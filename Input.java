ab4971a6f92cfcd8628dbf98a04541bfe911e9f5
Rohits3214 <rohitcse3214@gmail.com>
2025-01-24 22:38:37 +0530

    add new files

---
+++ Input.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Input {
+
+    public static void main(String[] args) {
+
+        Scanner scn  =  new Scanner(System.in);
+
+     System.out.print("enter first number:");
+     int a= scn.nextInt();
+
+
+    System.out.print("enter second number");
+    int b = scn.nextInt();
+
+    System.out.println("sum :"+(a +b));
+
+    scn.close();
+     
+    } 
+}
+
+
+
+        
+        
+        
+        
+        
+    
+    
+
---
+++ conditional.java
@@ -0,0 +1,18 @@
+public class conditional {
+
+    public static void main (String args[]){
+    String str ="rohit Singh";
+        int i =5;
+
+
+
+        if (i == 5){
+            i++;
+         System.out.println(str);
+       
+        
+    }
+    System.out.println("i="+i);
+}
+    
+}   
