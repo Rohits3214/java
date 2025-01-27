c04d22f5ee7c9609226ebef2888b0f220ed6e441
Rohits3214 <rohitcse3214@gmail.com>
2025-01-27 12:11:44 +0530

    for loop

---
+++ Palindromechecker.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class Palindromechecker {
+    public static void main(String[] args){
+
+        Scanner scn = new Scanner(System.in);
+
+        System.out.print("Enter a string to check if it is palindrome or not");
+
+
+    }
+    
+    
+}
