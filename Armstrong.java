4f30a61c1db9ebed9a568ad1e1121ccb35009d1f
Rohits3214 <rohitcse3214@gmail.com>
2025-02-05 11:08:05 +0530

    add

--- Armstrong.java
+++ Armstrong.java
@@ -3,23 +3,23 @@ import java.util.Scanner;
 public class Armstrong{
     
     public static void main(String[] args) {
-      Scanner scn = new Scanner(System.in);
+    Scanner scn = new Scanner(System.in); 
+     
+       System.out.println("Enter a number");
+       int number = scn.nextInt();
+       scn.close();
 
-      System.out.println("Enter a number");
-      int number = scn.nextInt();
-      scn.class();
-
-      if (isArmstrong(number)) {
-        System.out.println(number + " is  an  armstrong number");
-        
-      }else{
-        System.out.println(number + " is not an armstrong number");
-      }
-
-
-    }
-
-    public static boolean isArmstrong(int num){
+  if (isArmstrong(number)) {
+       System.out.println(number + " is  an  armstrong number");
+  
+   }else{
+  System.out.println(number + " is not an armstrong number");
+   
+  
+  }
+   
+}
+        public static boolean isArmstrong(int num){
         int originalNum = num;
         int sum = 0;
         int digits = String.valueOf(num).length();
@@ -28,10 +28,7 @@ public class Armstrong{
         int digit  = num % 10;
         sum += Math.pow(digit ,digits);
         num /= 10;
-       
-
-    }
+        }
     return sum == originalNum;
-
-    }
+ }
 }
