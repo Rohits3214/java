b5f9f66a516547db6e6506f4bc6e561d85bf45e2
Rohits3214 <rohitcse3214@gmail.com>
2025-02-05 10:51:24 +0530

    Armstrong

--- Table.java
+++ Table.java
@@ -5,10 +5,10 @@ public class Table {
     public static void main(String[] args) {
         
         Scanner scn = new Scanner(System.in);
-     int n =scn.nextInt();
+     int n = scn.nextInt();
 
      for (int i = 1; i < 11; i++) {
-        System.out.println(n + " X  "+  i  + " = " +(n*i));
+        System.out.println(n + "  X  "+  i  + " = " +(n*i));
         
     }
 }
