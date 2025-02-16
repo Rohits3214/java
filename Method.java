113b7405d3f2d7a774517afa4691a6015165f470
Rohits3214 <rohitcse3214@gmail.com>
2025-02-16 22:37:04 +0530

    pattern

---
+++ Method.java
@@ -0,0 +1,13 @@
+public class Method {
+    
+    static int addition(int a,int b){
+    return a + b;
+    }
+
+    public static void main(String[] args) {
+        int sum =addition(5,10);
+
+        System.out.println("sum is"+ sum);
+
+    }
+}
